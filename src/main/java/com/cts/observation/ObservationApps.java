14d3c288b17885b5f784868f48170db7ee5b6727
Ganesan Natarajan Shanmugam <ganesan.ns@cognizant.com>
2017-06-28 13:16:20 +0530

    Refactor App Name

--- src/main/java/com/cts/observation/ObservationApps.java
+++ src/main/java/com/cts/observation/ObservationApps.java
@@ -1,12 +1,12 @@
-package com.cts.template.template;
+package com.cts.observation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class ObservationApps {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApplication.class, args);
+		SpringApplication.run(ObservationApps.class, args);
 	}
 }
--- src/test/java/com/cts/observation/ObservationAppsTests.java
+++ src/test/java/com/cts/observation/ObservationAppsTests.java
@@ -1,4 +1,4 @@
-package com.cts.template.template;
+package com.cts.observation;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TemplateApplicationTests {
+public class ObservationAppsTests {
 
 	@Test
 	public void contextLoads() {
