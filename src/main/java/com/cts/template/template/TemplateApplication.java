6ceb9e30a6f90c7e034c7e47cb804979c4432574
Ganesan Natarajan Shanmugam <ganesan.ns@cognizant.com>
2017-06-28 13:03:09 +0530

    first commit

---
+++ src/main/java/com/cts/template/template/TemplateApplication.java
@@ -0,0 +1,12 @@
+package com.cts.template.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+}
